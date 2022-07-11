8df2394520cd59a4a94804c30b63f7cdc7ef20a0
amosi ally sanga <sangaamosi04@gmail.com>
2022-07-11 13:50:01 +0300

    contacts and about section added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,8 +9,8 @@ const Nav = () => {
         <div className="text-2xl uppercase ml-2 italic flex items-center">logo</div>
         <div className={`${styles.navlinks} ml-auto flex items-center justify-around`}>
           <Link href="/" >Home</Link>
-          <Link href="/contacts" >Contacts</Link>
-          <Link href="/about" >about</Link>
+          <Link href="#about" >about</Link>
+          <Link href="#contacts" >Contacts</Link>
         </div>
       </nav>
     </>
